a6e28124f5bf528cb4b4fddbb83ab8e678429951
Anton-mnc <antoine.mgy.mnc@gmail.com>
2023-02-02 15:26:35 +0100

    Commit test ng bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'top-colleagues';
 }
-
-import {Component} from '@angular/core';
-import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
-
-@Component({
-  selector: 'ngbd-collapse-horizontal',
-  standalone: true,
-  imports: [NgbCollapseModule],
-  templateUrl: './collapse-horizontal.html',
-})
-export class NgbdCollapseHorizontal {
-  public isCollapsed = false;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    C1Component,
   ],
   providers: [],
   bootstrap: [AppComponent]
